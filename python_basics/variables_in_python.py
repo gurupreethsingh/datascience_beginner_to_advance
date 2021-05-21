a90c98bfb3f9b12f6d0cb3b3b0b6223b2029ea22
Gurupreeth Singh <igurupreeth@gmail.com>
2021-05-21 15:44:39 +0530

    tokens

---
+++ python_basics/variables_in_python.py
@@ -0,0 +1,22 @@
+#Tokens :- the smallest part of any programming language
+"""
+1. identifiers
+2. literals
+3. variables
+4. keywords
+5. separators
+6. operators
+"""
+
+# identifiers :- are the names given to our variables and programs
+
+a = 10  # we have given 'a' as the name to identify the integer value 10
+print(a)
+
+#literals :- are the types of variables, the different data types
+a = 10   # interger type of literal
+b = 23.4   # float( decimal) type of literal
+c = 'a'    # character or string type of literal
+d = 'hello welcome to data science'   # string literal
+f = "welcome ecoders"   # string literal
+
